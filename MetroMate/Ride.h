d9c5780a760ee02488cdad9fdd1baff91acc639b
muhammedsherifff <midosherif25@gmail.com>
2024-04-20 21:43:18 +0200

    hatena cons

--- MetroMate/Ride.h
+++ MetroMate/Ride.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <string>
+#include <iostream>
+using namespace std;
 class Ride
 {
+public:
+	int id;
+	string source, destination;
+	int station_id;
+public:
+	Ride(int rideId, string src, string dest, int stationId)
+		: id(rideId), source(src), destination(dest), station_id(stationId) {}
 };
 
