26308ba07b0b55f93ffc9a3298982e8b34d28e47
muhammedsherifff <midosherif25@gmail.com>
2024-04-20 21:47:06 +0200

    constructor for data

--- MetroMate/Subscription.cpp
+++ MetroMate/Subscription.cpp
@@ -1,1 +1,15 @@
 #include "Subscription.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Subscription::Subscription(int id, string type, string subDate, string endDate, int remainingRides, int userId, int subId) {
+    ID = id;
+    Type = type;
+    Sub_datee = subDate;
+    End_datee = endDate;
+    remaining_rides = remainingRides;
+    user_idd = userId;
+    sub_idd = subId;
+}
+
