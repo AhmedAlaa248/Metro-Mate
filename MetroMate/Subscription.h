0f393078eb8ddf1dec18a6798818e3d6a88520e5
muhammedsherifff <midosherif25@gmail.com>
2024-04-20 21:44:53 +0200

    hatena data

--- MetroMate/Subscription.h
+++ MetroMate/Subscription.h
@@ -1,5 +1,19 @@
 #pragma once
+using namespace std;
+#include <iostream>
+#include <string>
 class Subscription
 {
+    int ID;
+    string Type;
+    string Sub_datee;
+    string End_datee;
+    int remaining_rides;
+    int user_idd;
+    int sub_idd;
+public:
+    Subscription(int id, string type, string subDate,
+        string endDate, int remainingRides, int userId,
+        int subId);
 };
 
