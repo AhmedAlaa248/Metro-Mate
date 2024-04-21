61bfbf3b470946050b67f06b8cefa3aeb7c93f6e
yasseramr-1 <127059373+yasseramr-1@users.noreply.github.com>
2024-04-21 04:41:26 +0200

    Update Station.h

--- MetroMate/Station.h
+++ MetroMate/Station.h
@@ -1,7 +1,13 @@
+
 #pragma once
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <limits>
+#include <algorithm>
 using namespace std;
 
 class Station
@@ -15,12 +21,15 @@ public:
 	long numOfSoldTickets;
 	long numOfSubscriptions; //increased when add subscription
 	long long numOfPassengers;
-
+	unordered_map<int, unordered_set<int>> adjacencyList;
+	vector<vector<int>> allPaths;
 	Station();
 	Station(string n, int line);
 	void rideStarted(string type);
-	void addConnection();
+	void addConnection(int station1, int station2);
+	void removeConnection(int station1, int station2);
+	void removeStation(int station);
+	void addStationWithConnections(int station);
 	void displaYDetails(string type);
 
 };
-
