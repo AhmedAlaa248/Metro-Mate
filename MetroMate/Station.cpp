f3708c9ae6bf153d2a5fbbd3c0cd076c5539a530
yasseramr-1 <127059373+yasseramr-1@users.noreply.github.com>
2024-04-21 04:42:36 +0200

    Update Station.cpp

--- MetroMate/Station.cpp
+++ MetroMate/Station.cpp
@@ -1,5 +1,5 @@
-#include "Station.h"
 
+#include "Station.h"
 Station::Station()
 {
 	id = nextId++;
@@ -24,38 +24,37 @@ void Station::rideStarted(string type)
 {
 	if (type == "Cash Wallet")
 		numOfSoldTickets++;
-	
+
 	numOfPassengers++;
 }
 
-void Station::addConnection()
-{
-	int choice;
-	do
-	{
-		string station;
-		cout << "1. To add connection.\n0. To exit.";
-		cin >> choice;
-		switch (choice)
-		{
-		case 0:
-			break;
-		case 1:
-			cout << "Enter the name of the other staion: ";
-			cin >> station;
-			/*for (int i = 0; i < stations.length(); i++)
-			{
-				if(stations[i].name == station)
-					//call the function and give it (id, stations[i].id);
-			}*/
-			break;
-		default:
-			cout << "Invalid choice\n";
-			break;
-		}
-	} while (choice);
+
+void Station::addConnection(int station1, int station2) {
+	adjacencyList[station1].insert(station2);
+	adjacencyList[station2].insert(station1);
 }
 
+void Station::removeConnection(int station1, int station2) {
+	adjacencyList[station1].erase(station2);
+	adjacencyList[station2].erase(station1);
+}
+
+void Station::removeStation(int station) {
+	adjacencyList.erase(station);
+	for (auto& pair : adjacencyList) {
+		pair.second.erase(station);
+	}
+}
+void Station::addStationWithConnections(int stationId) {
+	cout << "Enter connections for station " << stationId << " (Enter -1 to stop):" << endl;
+	int connection;
+	while (true) {
+		cout << "Enter connection: ";
+		cin >> connection;
+		if (connection == -1) break;
+		addConnection(stationId, connection);
+	}
+}
 void Station::displaYDetails(string type)
 {
 	cout << "Station details:\n";
@@ -67,6 +66,5 @@ void Station::displaYDetails(string type)
 		cout << "\nnumber of sold tickets: " + numOfSoldTickets;
 		cout << "\tnumber of subscriptions: " + numOfSubscriptions;
 		cout << "\tnumber of passengers: " + numOfPassengers;
-	}	
+	}
 }
-
